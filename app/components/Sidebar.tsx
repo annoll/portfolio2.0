4fdeb159bfe563700cbf359d356b547264f134f5
Annur12 <annolmanggona12@gmail.com>
2026-04-04 13:28:17 +0800

    Add Sidebar component and personal info

---
+++ app/components/Sidebar.tsx
@@ -0,0 +1,23 @@
+import { BadgeCheck, MapPin } from "lucide-react";
+
+export default function Sidebar() {
+  return (
+    <div className="p-10">
+      <div className="flex items-center gap-2">
+        <h1 className="text-stone-800 font-bold text-lg">Annol Manggon</h1>
+        <BadgeCheck className="fill-blue-500 text-white" size={16} />
+      </div>
+
+      <div className="flex items-center gap-1 mt-1">
+        <MapPin size={16} className="text-stone-500" />
+        <p className="text-stone-500 text-sm">Zamboanga City, Philippines</p>
+      </div>
+
+      <div>
+        <p className="text-stone-400 text-xs mt-0.5">
+          Junior Software Engineer
+        </p>
+      </div>
+    </div>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,15 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Poppins, Space_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const spaceMono = Space_Mono({
   subsets: ["latin"],
+  weight: ["400", "700"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const poppins = Poppins({
   subsets: ["latin"],
+  weight: ["400", "700"],
 });
 
 export const metadata: Metadata = {
@@ -23,10 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
+    <html lang="en" className={`${poppins.className} h-full antialiased`}>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
